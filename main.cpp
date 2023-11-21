9ae9aedd110e2540e7c345fe7f0adb9708ca1b38
Straple <hadayegor@gmail.com>
2023-11-21 10:40:04 +0300

    start task solving

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::ifstream cin("input.txt");
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+
+}
