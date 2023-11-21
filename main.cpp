06f5d35c114e01e9bd36c431da61ab2aab1413c2
Straple <hadayegor@gmail.com>
2023-11-21 14:50:30 +0300

    6132.76 points

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-    // std::ifstream cin("input.txt");
+    //std::ifstream cin("input.txt");
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
+
 
     // ===========
     // ==READING==
@@ -18,25 +19,25 @@ int main() {
     int R;
     cin >> N >> K >> T >> R;
 
-    // s0[k][r][n][t]
-    vector<vector<vector<vector<double>>>> s0(K, vector(R, vector(N, vector<double>(T))));
+    // s0[t][n][k][r]
+    vector<vector<vector<vector<double>>>> s0(T, vector(N, vector(K, vector<double>(R))));
     for (int t = 0; t < T; t++) {
         for (int k = 0; k < K; k++) {
             for (int r = 0; r < R; r++) {
                 for (int n = 0; n < N; n++) {
-                    cin >> s0[k][r][n][t];
+                    cin >> s0[t][n][k][r];
                 }
             }
         }
     }
 
-    // d[k][m][r][n]
-    vector<vector<vector<vector<double>>>> d(K, vector(N, vector(R, vector<double>(N))));
+    // d[n][r][k][m]
+    vector<vector<vector<vector<double>>>> d(N, vector(R, vector(K, vector<double>(N))));
     for (int k = 0; k < K; k++) {
         for (int r = 0; r < R; r++) {
             for (int m = 0; m < N; m++) {
                 for (int n = 0; n < N; n++) {
-                    cin >> d[k][m][r][n];
+                    cin >> d[n][r][k][m];
                 }
             }
         }
@@ -68,86 +69,8 @@ int main() {
         Queries[j].t1 = t1;
     }
 
-    // p[k][r][n][t]
-    vector<vector<vector<vector<double>>>> p(K, vector(R, vector(N, vector<double>(T))));
-
-    // NOLINTNEXTLINE
-    auto build_s_krnt = [&]() {
-        // s[k][r][n][t]
-        vector<vector<vector<vector<double>>>> s(K, vector(R, vector(N, vector<double>(T))));
-
-        for (int k = 0; k < K; k++) {
-            for (int r = 0; r < R; r++) {
-                for (int n = 0; n < N; n++) {
-                    for (int t = 0; t < T; t++) {
-
-                        double prod = 1;
-                        for (int m = 0; m < N; m++) {
-                            if (m != n) {
-                                prod *= std::exp(d[k][m][r][n] * (p[k][r][n][t] > 0 ? 1 : 0));
-                            }
-                        }
-                        prod *= p[k][r][n][t];
-                        prod *= s0[k][r][n][t];
-
-                        double sum = 1;
-                        for (int k1 = 0; k1 < K; k1++) {
-                            if (k1 != k) {
-                                for (int n1 = 0; n1 < N; n1++) {
-                                    if (n1 != n) {
-                                        sum += s0[k1][r][n][t] * p[k1][r][n1][t] * exp(-d[k1][n1][r][n]);
-                                    }
-                                }
-                            }
-                        }
-
-                        s[k][r][n][t] = prod / sum;
-                    }
-                }
-            }
-        }
-
-        return s;
-    };
-
-    auto build_s_knt = [&](vector<vector<vector<vector<double>>>> s) {
-        vector<vector<vector<double>>> s_cur(K, vector(N, vector<double>(T)));
-        for (int k = 0; k < K; k++) {
-            for (int n = 0; n < N; n++) {
-                for (int t = 0; t < T; t++) {
-                    double prod = 1;
-                    int count = 0;
-                    for (int r = 0; r < R; r++) {
-                        if ((p[k][r][n][t] > 0 ? 1 : 0) == 1) {
-                            count++;
-                            prod *= s[k][r][n][t];
-                        }
-                    }
-
-                    s_cur[k][n][t] = std::pow(prod, 1.0 / count);
-                }
-            }
-        }
-        return s_cur;
-    };
-
-    auto build_g = [&](vector<vector<vector<double>>> s) {
-        vector<double> g(J);
-        for (int j = 0; j < J; j++) {
-            int n = Queries[j].user_id;
-            int t0 = Queries[j].t0;
-            int t1 = Queries[j].t1;
-            for (int t = t0; t <= t1; t++) {
-                for (int k = 0; k < K; k++) {
-                    for (int r = 0; r < R; r++) {
-                        g[j] += (p[k][r][n][t] > 0 ? 1 : 0) * log2(1 + s[k][n][t]);
-                    }
-                }
-            }
-            g[j] *= 192;
-        }
-        return g;
-    };
+    // p[t][n][k][r]
+    vector<vector<vector<vector<double>>>> p(T, vector(N, vector(K, vector<double>(R))));
 
     // ============
     // ==SOLUTION==
@@ -193,25 +116,25 @@ int main() {
                 sum_weight += weight;
             }
 
-            for (int k = 0; k < K; k++) {
-                for (int r = 0; r < R; r++) {
-                    for (auto [weight, n]: kek) {
-                        p[k][r][n][t] = weight / sum_weight * 4;
+            for (auto [weight, n]: kek) {
+                for (int k = 0; k < K; k++) {
+                    for (int r = 0; r < R; r++) {
+                        p[t][n][k][r] = weight / sum_weight * 4;
                     }
                 }
             }
 
             for (int k = 0; k < K; k++) {
                 double sum = 0;
-                for (int r = 0; r < R; r++) {
-                    for (auto [weight, n]: kek) {
-                        sum += p[k][r][n][t];
+                for (auto [weight, n]: kek) {
+                    for (int r = 0; r < R; r++) {
+                        sum += p[t][n][k][r];
                     }
                 }
 
-                for (int r = 0; r < R; r++) {
-                    for (auto [weight, n]: kek) {
-                        p[k][r][n][t] *= min(1.0, (weight / sum_weight) * (R / sum));
+                for (auto [weight, n]: kek) {
+                    for (int r = 0; r < R; r++) {
+                        p[t][n][k][r] *= min(1.0, (weight / sum_weight) * (R / sum));
                     }
                 }
             }
@@ -219,60 +142,56 @@ int main() {
 
         // update g
 
-        vector<vector<vector<double>>> s(K, vector(R, vector<double>(N)));
-
-        for (auto [weight, n]: kek) {
-            for (int k = 0; k < K; k++) {
-                for (int r = 0; r < R; r++) {
-
-                    double prod = 1;
-                    for (int m = 0; m < N; m++) {
-                        if (m != n) {
-                            prod *= exp(d[k][m][r][n] * (p[k][r][n][t] > 0 ? 1 : 0));
-                        }
-                    }
-                    prod *= p[k][r][n][t];
-                    prod *= s0[k][r][n][t];
-
-                    double sum = 1;
-                    for (int k1 = 0; k1 < K; k1++) {
-                        if (k1 != k) {
-                            for (int n1 = 0; n1 < N; n1++) {
-                                if (n1 != n) {
-                                    sum += s0[k1][r][n][t] * p[k1][r][n1][t] * exp(-d[k1][n1][r][n]);
-                                }
-                            }
-                        }
-                    }
-
-                    s[k][r][n] = prod / sum;
-                }
-            }
-        }
-
-        vector<vector<double>> s_cur(K, vector<double>(N));
-        for (int k = 0; k < K; k++) {
-            for (auto [weight, n]: kek) {
-                double prod = 1;
-                int count = 0;
-                for (int r = 0; r < R; r++) {
-                    if ((p[k][r][n][t] > 0 ? 1 : 0) == 1) {
-                        count++;
-                        prod *= s[k][r][n];
-                    }
-                }
-
-                s_cur[k][n] = std::pow(prod, 1.0 / count);
-            }
-        }
-
         vector<int> need_delete;
         for (auto &[n, data]: users) {
+            double sum = 0;
             for (int k = 0; k < K; k++) {
-                for (int r = 0; r < R; r++) {
-                    data.g += 192 * (p[k][r][n][t] > 0 ? 1 : 0) * log2(1 + s_cur[k][n]);
+                double s_cur;
+                {
+                    double prod = 1;
+                    int count = 0;
+                    for (int r = 0; r < R; r++) {
+                        if (p[t][n][k][r] > 0) {
+                            count++;
+
+                            double s;
+                            {
+                                double prod = 1;
+                                for (int m = 0; m < N; m++) {
+                                    if (m != n) {
+                                        prod *= exp(d[n][r][k][m] * (p[t][n][k][r] > 0 ? 1 : 0));
+                                    }
+                                }
+                                prod *= p[t][n][k][r];
+                                prod *= s0[t][n][k][r];
+
+                                double sum = 1;
+
+                                for (int k1 = 0; k1 < K; k1++) {
+                                    if (k1 != k) {
+                                        for (int m = 0; m < N; m++) {
+                                            if (m != n) {
+                                                sum += s0[t][n][k1][r] * p[t][m][k1][r] * exp(-d[n][r][k1][m]);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                s = prod / sum;
+                            }
+                            prod *= s;
+                        }
+                    }
+
+                    s_cur = std::pow(prod, 1.0 / count);
                 }
+                int count = 0;
+                for (int r = 0; r < R; r++) {
+                    count += p[t][n][k][r] > 0;
+                }
+                sum += count * log2(1 + s_cur);
             }
+            data.g += 192 * sum;
 
             // мы уже отправили все
             if (data.g >= Queries[data.j].TBS) {
@@ -294,32 +213,12 @@ int main() {
     // ==OUTPUT==
     // ==========
 
-    {
-        /*for (int t = 0; t < T; t++) {
-            for (int k = 0; k < K; k++) {
-                for (int r = 0; r < R; r++) {
-                    for (int n = 0; n < N; n++) {
-
-                        cin >> p[k][r][n][t];
-                    }
-                }
-            }
-        }*/
-
-        /*auto g = build_g(build_s_knt(build_s_krnt()));
-
-        for (int j = 0; j < J; j++) {
-            cout << g[j] << ' ' << Queries[j].TBS << ' ' << (g[j] >= Queries[j].TBS) << '\n';
-        }
-        cout << '\n';*/
-    }
-
     cout << fixed << setprecision(10);
     for (int t = 0; t < T; t++) {
         for (int k = 0; k < K; k++) {
             for (int r = 0; r < R; r++) {
                 for (int n = 0; n < N; n++) {
-                    cout << p[k][r][n][t] << ' ';
+                    cout << p[t][n][k][r] << ' ';
                 }
                 cout << '\n';
             }
